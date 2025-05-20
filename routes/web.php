39f1e5006b39d745694a2b3aad7f52955e9509fc
Cindy2104 <cindyrahmarjulis2121@gmail.com>
2025-05-20 09:49:36 +0700

    homepage

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MovieController;
+use App\Http\Controllers\CategoryController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [MovieController::class, 'homepage']);
+Route::resource('/category', CategoryController::class);
+
+
